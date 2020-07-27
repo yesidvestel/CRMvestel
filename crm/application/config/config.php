3adf761df34f0d5c368a578d3507da19a7ea65a7
yesidvestel <58863813+yesidvestel@users.noreply.github.com>
2020-07-27 10:01:59 -0500

    ingreso a usuarios

--- crm/application/config/config.php
+++ crm/application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 
-$config['base_url'] = 'http://localhost/application_setup/crm/';
+$config['base_url'] = 'http://localhost/CRMvestel/crm/';
 
 /*
 |--------------------------------------------------------------------------
