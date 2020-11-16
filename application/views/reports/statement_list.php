8a54db2de9379959120ec2a88e4a8d77bfdb5bbd
Duber1234 <dfpesca@misena.edu.co>
2020-11-16 13:43:45 -0500

    desarrollo de conteo de planes 1,2,3,5,10 megas o television... y no mostrar si no esta

--- application/views/reports/statement_list.php
+++ application/views/reports/statement_list.php
@@ -135,36 +135,76 @@
 					</tr>
 				</thead>
 				<tbody>
+
+					<?php 
+					$var_cuenta_planes=array("1Mega"=>0,"2Megas"=>0,"3Megas"=>0,"5Megas"=>0,"10Megas"=>0,"Television"=>0); 
+					foreach ($lista as $key => $value) { 
+						$invoice = $this->db->get_where("invoices",array("tid"=>$value['tid']))->row(); 
+					
+						if($invoice->combo=="1Mega"){
+						 		$var_cuenta_planes['1Mega']++;
+
+						}else if($invoice->combo=="2Megas"){
+								$var_cuenta_planes['2Megas']++;
+
+						}else if($invoice->combo=="3Megas"){
+								$var_cuenta_planes['3Megas']++;
+
+						}else if($invoice->combo=="5Megas"){
+								$var_cuenta_planes['5Megas']++;
+
+						}else if($invoice->combo=="10Megas"){
+								$var_cuenta_planes['10Megas']++;
+
+						}else{
+								$var_cuenta_planes['Television']++;
+						}
+
+					 } 
+					 	
+					 ?>
+					<?php if($var_cuenta_planes['1Mega']!=0){  ?>
 					<tr>
 						<td>Internet 1MG</td>
-						<td style="text-align: center">0</td>
+						<td style="text-align: center"><?=$var_cuenta_planes['1Mega']?></td>
 						<td style="text-align: center">0</td>
 					</tr>
+					<?php } ?>
+					<?php if($var_cuenta_planes['2Megas']!=0){  ?>
 					<tr>
 						<td>Internet 2MG</td>
-						<td style="text-align: center">0</td>
+						<td style="text-align: center"><?=$var_cuenta_planes['2Megas']?></td>
 						<td style="text-align: center">0</td>
 					</tr>
+					<?php } ?>
+					<?php if($var_cuenta_planes['3Megas']!=0){  ?>
 					<tr>
 						<td>Internet 3MG</td>
-						<td style="text-align: center">0</td>
+						<td style="text-align: center"><?=$var_cuenta_planes['3Megas']?></td>
 						<td style="text-align: center">0</td>
 					</tr>
+					<?php } ?>
+					<?php if($var_cuenta_planes['5Megas']!=0){  ?>
 					<tr>
 						<td>Internet 5MG</td>
-						<td style="text-align: center">0</td>
+						<td style="text-align: center"><?=$var_cuenta_planes['5Megas']?></td>
 						<td style="text-align: center">0</td>
 					</tr>
+					<?php } ?>
+					<?php if($var_cuenta_planes['10Megas']!=0){  ?>
 					<tr>
 						<td>Internet 10MG</td>
-						<td style="text-align: center">0</td>
+						<td style="text-align: center"><?=$var_cuenta_planes['10Megas']?></td>
 						<td style="text-align: center">0</td>
 					</tr>
+					<?php } ?>
+					<?php if($var_cuenta_planes['Television']!=0){  ?>
 					<tr>
-						<td>Internet 10MG</td>
-						<td style="text-align: center">0</td>
+						<td>Television</td>
+						<td style="text-align: center"><?=$var_cuenta_planes['Television']?></td>
 						<td style="text-align: center">0</td>
 					</tr>
+					<?php } ?>
 					<tr>
 						<th class="pie">TOTAL MENSUALIDADES</th>
 						<th class="pie">0</th>
