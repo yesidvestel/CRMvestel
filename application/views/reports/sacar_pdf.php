f271fa8142ae512c6466b95800ad6d05300b393e
Duber1234 <dfpesca@misena.edu.co>
2020-12-07 18:40:26 -0500

    arreglos visuales... pdf

--- application/views/reports/sacar_pdf.php
+++ application/views/reports/sacar_pdf.php
@@ -8,6 +8,7 @@ $mpdf = new \Mpdf\Mpdf([
 $mpdf->SetTitle('Lista de Usuarios');
 
 $contenidoTabla="
+
 <div style='box-shadow: 0px 2px 0px rgba(0, 0, 0, 0.05);margin-bottom: 1.875rem;border-radius: 0;padding: 1.5rem'>
 <h6 style='font-size: 1rem;margin-bottom: 0.5rem;font-family: inherit;font-weight: 500;line-height: 1.2;color: inherit;margin-top: 0;'>Estado de Caja : </h6>
 <hr style='margin-top: 1rem;margin-bottom: 1rem;border: 0;border-top: 1px solid rgba(0, 0, 0, 0.1);'>
@@ -179,7 +180,7 @@ $contenidoTabla="
 		</td>
 	</tr>
 	<tr>
-		<td>
+		<td style='vertical-align: top;'>
 			<h6 style='font-size: 1rem;margin-bottom: 0.5rem;font-family: inherit;font-weight: 500;line-height: 1.2;color: inherit;margin-top: 10px;'>Resumen por Servicios</h6>
 			<table style='border-collapse: collapse;border: 1px solid #5F5F5F;border-spacing: 2px;'>
 					<thead>
@@ -248,9 +249,40 @@ $contenidoTabla="
 						</tr>
 					</tfoot>
 			</table>
+			<br>
+			<h6 style='font-size: 1rem;margin-bottom: 0.5rem;font-family: inherit;font-weight: 500;line-height: 1.2;color: inherit;margin-top: 10px;'>Resumen de cargos cobrados por meses</h6>
+			<table style='border-collapse: collapse;border: 1px solid #5F5F5F;border-spacing: 2px;'>
+					<thead>
+						<tr >
+							<th style='background: #555;color: #fff;text-transform: uppercase;text-align: center;font-size: 14px;padding: 10px;'>
+							DESCRIPCION</th>
+							<th style='background: #555;color: #fff;text-transform: uppercase;text-align: center;font-size: 14px;padding: 10px;'>
+							CANT</th>
+							<th style='background: #555;color: #fff;text-transform: uppercase;text-align: center;font-size: 14px;padding: 10px;'>
+							MONTO</th>	
+						</tr>
+					</thead>
+					<tbody>
+						<tr >
+							<td style='border-bottom: 2px solid #111;color: #333;font-size: 12px;padding: 10px;'>Diciembre 2020</td><td style='text-align: center;border-bottom: 2px solid #111;color: #333;font-size: 12px;padding: 10px;'>12</td><td style='text-align: center;border-bottom: 2px solid #111;color: #333;font-size: 12px;padding: 10px;'>$ 545.000</td>
+						</tr>
+						<tr>
+							<td style='border-bottom: 2px solid #111;color: #333;font-size: 12px;padding: 10px;'>Noviembre 2020</td><td style='text-align: center;border-bottom: 2px solid #111;color: #333;font-size: 12px;padding: 10px;'>0</td><td style='text-align: center;border-bottom: 2px solid #111;color: #333;font-size: 12px;padding: 10px;'>$ 0</td>
+						</tr>
+					
+						
+					</tbody>
+					<tfoot>
+						<tr>
+							<th style='background: #E1E1E1;color: #000000;text-transform: uppercase;text-align: center;font-size: 10px;padding: 10px;' >TOTAL COBRANZA<br>POR MESES</th>
+							<th style='background: #E1E1E1;color: #000000;text-transform: uppercase;text-align: center;font-size: 10px;padding: 10px;'>12</th>
+							<th style='background: #E1E1E1;color: #000000;text-transform: uppercase;text-align: center;font-size: 10px;padding: 10px;'>$ 545.000</th>			
+						</tr>
+					</tfoot>
+			</table>
 		</td>
 		<td></td>
-		<td>
+		<td style='vertical-align: top;'>
 			<h6 style='font-size: 1rem;margin-bottom: 0.5rem;font-family: inherit;font-weight: 500;line-height: 1.2;color: inherit;margin-top: 10px;'>Resumen de cargos cobrados <br>por meses INTERNET</h6>
 			<table style='border-collapse: collapse;border: 1px solid #5F5F5F;border-spacing: 2px;'>
 					<thead>
@@ -346,37 +378,8 @@ $contenidoTabla="
 		</td>
 	</tr>
 	<tr>
-		<td><br>
-			<h6 style='font-size: 1rem;margin-bottom: 0.5rem;font-family: inherit;font-weight: 500;line-height: 1.2;color: inherit;margin-top: 10px;'>Resumen de cargos cobrados por meses</h6>
-			<table style='border-collapse: collapse;border: 1px solid #5F5F5F;border-spacing: 2px;'>
-					<thead>
-						<tr >
-							<th style='background: #555;color: #fff;text-transform: uppercase;text-align: center;font-size: 14px;padding: 10px;'>
-							DESCRIPCION</th>
-							<th style='background: #555;color: #fff;text-transform: uppercase;text-align: center;font-size: 14px;padding: 10px;'>
-							CANT</th>
-							<th style='background: #555;color: #fff;text-transform: uppercase;text-align: center;font-size: 14px;padding: 10px;'>
-							MONTO</th>	
-						</tr>
-					</thead>
-					<tbody>
-						<tr >
-							<td style='border-bottom: 2px solid #111;color: #333;font-size: 12px;padding: 10px;'>Diciembre 2020</td><td style='text-align: center;border-bottom: 2px solid #111;color: #333;font-size: 12px;padding: 10px;'>12</td><td style='text-align: center;border-bottom: 2px solid #111;color: #333;font-size: 12px;padding: 10px;'>$ 545.000</td>
-						</tr>
-						<tr>
-							<td style='border-bottom: 2px solid #111;color: #333;font-size: 12px;padding: 10px;'>Noviembre 2020</td><td style='text-align: center;border-bottom: 2px solid #111;color: #333;font-size: 12px;padding: 10px;'>0</td><td style='text-align: center;border-bottom: 2px solid #111;color: #333;font-size: 12px;padding: 10px;'>$ 0</td>
-						</tr>
-					
-						
-					</tbody>
-					<tfoot>
-						<tr>
-							<th style='background: #E1E1E1;color: #000000;text-transform: uppercase;text-align: center;font-size: 10px;padding: 10px;' >TOTAL COBRANZA<br>POR MESES</th>
-							<th style='background: #E1E1E1;color: #000000;text-transform: uppercase;text-align: center;font-size: 10px;padding: 10px;'>12</th>
-							<th style='background: #E1E1E1;color: #000000;text-transform: uppercase;text-align: center;font-size: 10px;padding: 10px;'>$ 545.000</th>			
-						</tr>
-					</tfoot>
-			</table>
+		<td>
+			
 		</td>
 		<td></td>
 		<td>
