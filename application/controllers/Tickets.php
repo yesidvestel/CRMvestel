f42c8e2bd130659a4da947c7c686aea33f60c359
yesidvestel <58863813+yesidvestel@users.noreply.github.com>
2021-05-13 15:21:49 -0500

    TICKET
    excel añadido documento

--- application/controllers/Tickets.php
+++ application/controllers/Tickets.php
@@ -386,7 +386,8 @@ class Tickets Extends CI_Controller
         'Fecha Creada' => 'string',
 		'Fecha Cierre' => 'string',
         'Abonado' => 'string',
-		'Usuario' => 'string',			 
+		'Usuario' => 'string',
+		'Documento' => 'string',
         'Estado' => 'string', 
         'Observacion' => 'string',
 		'Factura #' => 'string',			 
@@ -428,6 +429,7 @@ class Tickets Extends CI_Controller
 ['font'=>'Arial','font-style'=>'bold','font-size'=>'12',"fill"=>"#BDD7EE",'halign'=>'center'],
 ['font'=>'Arial','font-style'=>'bold','font-size'=>'12',"fill"=>"#BDD7EE",'halign'=>'center'],
 ['font'=>'Arial','font-style'=>'bold','font-size'=>'12',"fill"=>"#BDD7EE",'halign'=>'center'],
+['font'=>'Arial','font-style'=>'bold','font-size'=>'12',"fill"=>"#BDD7EE",'halign'=>'center'],
 ));
     
     //write rows to sheet1
@@ -441,7 +443,7 @@ class Tickets Extends CI_Controller
         if($tickets->fecha_final == '0000-00-00' || $tickets->fecha_final == '' || $tickets->fecha_final == null){
             $tickets->fecha_final="Sin Realizar";
         }
-            $writer->writeSheetRow('Tickets ',array($tickets->codigo,$tickets->subject ,$tickets->detalle, $tickets->created,$tickets->fecha_final,$tickets->abonado ,$tickets->name.' '.$tickets->unoapellido,$tickets->status,$obsv2,$tickets->id_factura,$tickets->asignado,$tickets->nomenclatura.' '.$tickets->numero1.$tickets->adicionauno.' # '.$tickets->numero2.$tickets->adicional2.' - '.$tickets->numero3,$tickets->barrio,$tickets->ciudad));
+            $writer->writeSheetRow('Tickets ',array($tickets->codigo,$tickets->subject ,$tickets->detalle, $tickets->created,$tickets->fecha_final,$tickets->abonado ,$tickets->name.' '.$tickets->unoapellido,$tickets->documento,$tickets->status,$obsv2,$tickets->id_factura,$tickets->asignado,$tickets->nomenclatura.' '.$tickets->numero1.$tickets->adicionauno.' # '.$tickets->numero2.$tickets->adicional2.' - '.$tickets->numero3,$tickets->barrio,$tickets->ciudad));
         
     }
         
