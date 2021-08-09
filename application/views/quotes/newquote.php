730a67644519c483cc6c599be78606ff7743b35b
yesidvestel <58863813+yesidvestel@users.noreply.github.com>
2021-08-09 16:56:03 -0500

    reconexion

--- application/views/quotes/newquote.php
+++ application/views/quotes/newquote.php
@@ -456,7 +456,7 @@
         });
     });
 	// selecion de orden
-	var perfil_servicio = new Array ("...","AgregarInternet","AgregarTelevision","Reconexion Internet","Bajar 15 Mg","Bajar 10 Mg","Bajar 5 Mg","Cambio de equipo","Corte Combo","Corte Internet","Corte Television","Equipo adicional","Instalacion","Punto nuevo","Subir 5 Mg","Subir 10 Mg","Subir 15 Mg","Subir 20 Mg","Suspension Combo","Suspension Internet","Suspension Television","Traslado","Toma Adicional");
+	var perfil_servicio = new Array ("...","AgregarInternet","AgregarTelevision","Reconexion Internet","Reconexion Television","Bajar 15 Mg","Bajar 10 Mg","Bajar 5 Mg","Cambio de equipo","Corte Combo","Corte Internet","Corte Television","Equipo adicional","Instalacion","Punto nuevo","Subir 5 Mg","Subir 10 Mg","Subir 15 Mg","Subir 20 Mg","Suspension Combo","Suspension Internet","Suspension Television","Traslado","Toma Adicional");
 	var perfil_reclamo = new Array ("...","Revision_de_Internet","Revision_de_television","Otros");	
 							//crear funcion que ejecute el cambio
 							function cambia(){
