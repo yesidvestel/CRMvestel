1ed43726eb0878b01b656fb05414a769c5dfb703
yesidvestel <58863813+yesidvestel@users.noreply.github.com>
2021-08-09 08:30:54 -0500

    ELIMINAR EQUIPOS

--- application/views/products/equipos.php
+++ application/views/products/equipos.php
@@ -167,7 +167,7 @@
             </div>
             <div class="modal-footer">
                 <input type="hidden" id="object-id" value="">
-                <input type="hidden" id="action-url" value="products/delete_i">
+                <input type="hidden" id="action-url" value="products/delete_e">
                 <button type="button" data-dismiss="modal" class="btn btn-primary"
                         id="delete-confirm"><?php echo $this->lang->line('Delete') ?></button>
                 <button type="button" data-dismiss="modal"
