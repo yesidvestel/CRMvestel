91ef97fcf03d21ac06d68cd4057f032da0a7f8ae
yesidvestel <58863813+yesidvestel@users.noreply.github.com>
2021-11-24 17:24:08 -0500

    PERMISOS1

--- application/views/employee/add.php
+++ application/views/employee/add.php
@@ -67,7 +67,370 @@
                                             </select>
                                         </div>
                                     </div>
+									<div class="form-group row">
 
+                                        <label class="col-sm-2 col-form-label"
+                                               for="name"><?php echo $this->lang->line('') ?>Permisos</label>
+
+                                        <div class="col-sm-10">
+                                            <table border="0">
+											  <tbody>
+												<tr>
+													<td><input type="checkbox" name="co"></input></td>
+												  	<td align="lift" colspan="2">COBRANZA</td>
+													<td><input type="checkbox" name="red"></input></td>
+												  	<td align="lift" colspan="2">REDES</td>
+													<td><input type="checkbox" name="inv"></input></td>
+												  	<td align="lift" colspan="2">INVENTARIOS</td>
+												</tr>
+												<tr>
+												  <td align="right"></td>
+												  <td><input type="checkbox" name="coape"></input></td>
+												  <td align="lift">Apertura</td>
+												  <td align="right"></td>
+												  <td><input type="checkbox" name="reding"></input></td>
+												  <td align="lift">Ingresar equipo</td>
+												  <td align="right"></td>
+												  <td><input type="checkbox" name="inving"></input></td>
+												  <td align="lift">Ingresar material</td>
+												</tr>
+												<tr>
+												  	<td align="right"></td>
+												  	<td><input type="checkbox" name="conue"></input></td>
+												  	<td align="lift">Nueva Factura</td>
+												  	<td align="right"></td>
+												  	<td><input type="checkbox" name="redadm"></input></td>
+													<td align="lift">Administrar equipos</td>
+													<td align="right"></td>
+												  	<td><input type="checkbox" name="invadm"></input></td>
+												  	<td align="lift">Administrar material</td>
+												</tr>
+												<tr>
+												  	<td align="right"></td>
+												  	<td><input type="checkbox" name="coadm"></input></td>
+												  	<td align="lift">Administrar facturas</td>
+												  	<td align="right"></td>
+												  	<td><input type="checkbox" name="redbod"></input></td>
+												  	<td align="lift">Bodega de equipos</td>
+													<td align="right"></td>
+													<td><input type="checkbox" name="invcat"></input></td>
+												  	<td align="lift">Categorias material</td
+												</tr>
+												<tr>
+												  	<td align="right"></td>
+												  	<td><input type="checkbox" name="cocie"></input></td>
+												  	<td align="lift">Cierre</td>
+												  	<td><input type="checkbox" name="com"></input></td>
+												  	<td align="lift" colspan="2">ORDEN DE COMPRA</td>
+													<td align="right"></td>
+													<td><input type="checkbox" name="invalm"></input></td>
+												  	<td align="lift">Almacenes</td>
+												</tr>
+												<tr>
+												  	<td align="right"></td>
+												  	<td><input type="checkbox" name="cofa"></input></td>
+												  	<td align="lift">Facturacion</td>
+												  	<td align="right"></td>
+												  	<td><input type="checkbox" name="comnue"></input></td>
+												  	<td align="lift">Nueva orden</td>
+													<td align="right"></td>
+													<td><input type="checkbox" name="invtrs"></input></td>
+												  	<td align="lift">Traspasos</td>
+												</tr>
+												<tr>
+												  	<td align="right"></td>
+												  	<td><input type="checkbox" name="cofae"></input></td>
+												  	<td align="lift">Facturacion electronica</td>
+												  	<td align="right"></td>
+												  	<td><input type="checkbox" name="comadm"></input></td>
+												  	<td align="lift">Administrar ordenes</td>
+													<td><input type="checkbox" name="emp"></input></td>
+												  	<td align="lift" colspan="2">EMPLEADOS</td>
+												</tr>
+												<tr>
+													<td><input type="checkbox" name="us"></input></td>
+												  	<td align="lift" colspan="2">USUARIOS</td>
+												  	<td><input type="checkbox" name="tes"></input></td>
+												  	<td align="lift" colspan="2">TESORERIA</td>
+												  	<td><input type="checkbox" name="comp"></input></td>
+												  	<td align="lift" colspan="2">COMPLEMENTOS</td>
+												</tr>
+												<tr>
+												  	<td align="right"></td>
+												  	<td><input type="checkbox" name="usnue"></input></td>
+												  	<td align="lift">Nuevo usuario</td>
+												  	<td align="right"></td>
+												  	<td><input type="checkbox" name="testran"></input></td>
+												  	<td align="lift">Ver transacciones</td>
+													<td align="right"></td>
+													<td><input type="checkbox" name="`comprec"></input></td>
+												  	<td align="lift">Recaptchat</td>
+												</tr>
+												<tr>
+												  	<td align="right"></td>
+												  	<td><input type="checkbox" name="usadm"></input></td>
+												  	<td align="lift">Administrar usuarios</td>
+												  	<td align="right"></td>
+												  	<td><input type="checkbox" name="tesanu"></input></td>
+												  	<td align="lift">Ver anulaciones</td>
+													<td align="right"></td>
+													<td><input type="checkbox" name="compurl"></input></td>
+												  	<td align="lift">Url corta</td>
+												</tr>
+												<tr>
+												  	<td align="right"></td>
+												  	<td><input type="checkbox" name="usgru"></input></td>
+												  	<td align="lift">Administrar grupos</td>
+												  	<td align="right"></td>
+												  	<td><input type="checkbox" name="tesnuetransac"></input></td>
+												  	<td align="lift">Nueva trasaccion</td>
+													<td align="right"></td>
+													<td><input type="checkbox" name="comptwi"></input></td>
+												  	<td align="lift">Twilio</td>
+												</tr>
+												<tr>
+													<td><input type="checkbox" name="tik"></input></td>
+												  	<td align="lift" colspan="2">TICKET</td>
+													<td align="right"></td>
+												  	<td><input type="checkbox" name="tesnuetransfer"></input></td>
+												  	<td align="lift">Nueva transferencia</td>
+													<td align="right"></td>
+													<td><input type="checkbox" name="compcurr"></input></td>
+												  	<td align="lift">Currency</td>
+												</tr>
+												<tr>
+												  	<td align="right"></td>
+												  	<td><input type="checkbox" name="tiknue"></input></td>
+												  	<td align="lift">Nuevo ticket</td>
+												  	<td align="right"></td>
+												  	<td><input type="checkbox" name="tesing"></input></td>
+												  	<td align="lift">Ingresos</td>
+												  	<td><input type="checkbox" name="pla"></input></td>
+												  	<td align="lift" colspan="2">PLANTILLAS</td>
+												</tr>
+												<tr>
+												  	<td align="right"></td>
+												  	<td><input type="checkbox" name="tikadm"></input></td>
+												  	<td align="lift">Administrar entrada</td>
+												  	<td align="right"></td>
+												  	<td><input type="checkbox" name="tesgas"></input></td>
+													<td align="lift">Gastos</td>
+													<td align="right"></td>
+												  	<td><input type="checkbox" name="placor"></input></td>
+												  	<td align="lift">Correo</td>
+												</tr>
+												<tr>
+												  	<td><input type="checkbox" name="mo"></input></td>
+												  	<td align="lift" colspan="2">MOVILES</td>
+												  	<td align="right"></td>
+												  	<td><input type="checkbox" name="testransfer"></input></td>
+												  	<td align="lift">Transferencias</td>
+													<td align="right"></td>
+													<td><input type="checkbox" name="plamen"></input></td>
+												  	<td align="lift">Mensajes</td
+												</tr>
+												<tr>
+												  	<td align="right"></td>
+												  	<td><input type="checkbox" name="monue"></input></td>
+												  	<td align="lift">Nueva movil</td>
+												  	<td><input type="checkbox" name="dat"></input></td>
+												  	<td align="lift" colspan="2">DATOS E INFORMES</td>
+													<td align="right"></td>
+													<td><input type="checkbox" name="platem"></input></td>
+												  	<td align="lift">Temas</td>
+												</tr>
+												<tr>
+												  	<td align="right"></td>
+												  	<td><input type="checkbox" name="moadm"></input></td>
+												  	<td align="lift">Administrar moviles</td>
+												  	<td align="right"></td>
+												  	<td><input type="checkbox" name="datest"></input></td>
+												  	<td align="lift">Estadisticas</td>
+													<td><input type="checkbox" name="conf"></input></td>
+												  	<td align="lift" colspan="2">CONFIGURACIONES</td>
+												</tr>
+												<tr>
+												  	<td><input type="checkbox" name="pro"></input></td>
+												  	<td align="lift" colspan="2">PROVEEDORES</td>
+												  	<td align="right"></td>
+												  	<td><input type="checkbox" name="datdec"></input></td>
+												  	<td align="lift">Declaraciones</td>
+													<td align="right"></td>
+												  	<td><input type="checkbox" name="confemp"></input></td>
+												  	<td align="lift">Empresa</td>
+												</tr>
+												<tr>
+													<td align="right"></td>
+												  	<td><input type="checkbox" name="pronue"></input></td>
+												  	<td align="lift">Nuevo proveedor</td>
+												  	<td align="right"></td>
+												  	<td><input type="checkbox" name="datrep"></input></td>
+												  	<td align="lift">Reporte tecnico</td>
+												  	<td align="right"></td>
+												  	<td><input type="checkbox" name="conffa"></input></td>
+												  	<td align="lift">Facturacion y lenguaje</td>
+												</tr>
+												<tr>
+												  	<td align="right"></td>
+												  	<td><input type="checkbox" name="proadm"></input></td>
+												  	<td align="lift">Administrar proveedor</td>
+												  	<td align="right"></td>
+												  	<td><input type="checkbox" name="datusu"></input></td>
+												  	<td align="lift">Usu. declaraciones</td>
+													<td align="right"></td>
+													<td><input type="checkbox" name="confmon"></input></td>
+												  	<td align="lift">Moneda</td>
+												</tr>
+												<tr>
+												  	<td><input type="checkbox" name="enc"></input></td>
+												  	<td align="lift" colspan="2">ENCUESTAS</td>
+												  	<td align="right"></td>
+												  	<td><input type="checkbox" name="datusu"></input></td>
+												  	<td align="lift">Prov. declaraciones</td>
+													<td align="right"></td>
+													<td><input type="checkbox" name="conffec"></input></td>
+												  	<td align="lift">Formato fecha</td>
+												</tr>
+												<tr>
+												  	<td align="right"></td>
+												  	<td><input type="checkbox" name="encllam"></input></td>
+												  	<td align="lift">Lista de llamadas</td>
+												  	<td align="right"></td>
+												  	<td><input type="checkbox" name="dating"></input></td>
+												  	<td align="lift">Calcular ingresos</td>
+													<td align="right"></td>
+													<td><input type="checkbox" name="confcat"></input></td>
+												  	<td align="lift">Categorias transaccion</td>
+												</tr>
+												<tr>
+													<td align="right"></td>
+												  	<td><input type="checkbox" name="encnue"></input></td>
+												  	<td align="lift">Nueva encuesta</td>
+													<td align="right"></td>
+												  	<td><input type="checkbox" name="datgas"></input></td>
+												  	<td align="lift">Calcular gastos</td>
+													<td align="right"></td>
+													<td><input type="checkbox" name="confmet"></input></td>
+												  	<td align="lift">Fijar metas</td>
+												</tr>
+												<tr>
+												  <td align="right"></td>
+												  <td><input type="checkbox" name="encenc"></input></td>
+												  <td align="lift">Lista encuestas</td>
+												  <td align="right"></td>
+												  <td><input type="checkbox" name="dattrans"></input></td>
+												  <td align="lift">Trans. clientes</td>
+												  <td align="right"></td>
+												  <td><input type="checkbox" name="confrest"></input></td>
+												  <td align="lift">Api rest</td>
+												</tr>
+												<tr>
+												  	<td align="right"></td>
+												  	<td><input type="checkbox" name="encats"></input></td>
+												  	<td align="lift">Nueva ATS</td>
+												  	<td align="right"></td>
+												  	<td><input type="checkbox" name="datimp"></input></td>
+													<td align="lift">Impuestos</td>
+													<td align="right"></td>
+												  	<td><input type="checkbox" name="confcorr"></input></td>
+												  	<td align="lift">Correo</td>
+												</tr>
+												<tr>
+												  	<td align="right"></td>
+												  	<td><input type="checkbox" name="encatslit"></input></td>
+												  	<td align="lift">Lista ATS</td>
+												  	<td><input type="checkbox" name="not"></input></td>
+												  	<td align="lift" colspan="2">NOTAS</td>
+													<td align="right"></td>
+													<td><input type="checkbox" name="confterm"></input></td>
+												  	<td align="lift">Termino facturacion</td
+												</tr>
+												<tr>
+												  	<td><input type="checkbox" name="proy"></input></td>
+												  	<td align="lift" colspan="2">PROYECTOS</td>
+												  	<td><input type="checkbox" name="cal"></input></td>
+												  	<td align="lift" colspan="2">CALENDARIO</td>
+													<td align="right"></td>
+													<td><input type="checkbox" name="confaut"></input></td>
+												  	<td align="lift">Trabajo automatico</td>
+												</tr>
+												<tr>
+												  	<td align="right"></td>
+												  	<td><input type="checkbox" name="proynue"></input></td>
+												  	<td align="lift">Nuevo proyecto</td>
+												  	<td><input type="checkbox" name="doct"></input></td>
+												  	<td align="lift" colspan="2">DOCUMENTOS</td>
+													<td align="right"></td>
+													<td><input type="checkbox" name="confseg"></input></td>
+												  	<td align="lift">Seguridad</td>
+												</tr>
+												<tr>
+												  	<td align="right"></td>
+												  	<td><input type="checkbox" name="proyadm"></input></td>
+												  	<td align="lift">Administrar proyectos</td>
+												  	<td><input type="checkbox" name="pag"></input></td>
+												  	<td align="lift" colspan="2">CONFIGURAR PAGO</td>
+													<td align="right"></td>
+													<td><input type="checkbox" name="conftem"></input></td>
+												  	<td align="lift">Tema</td>
+												</tr>
+												<tr>
+													<td><input type="checkbox" name="cuen"></input></td>
+												  	<td align="lift" colspan="2">CUENTAS</td>
+												  	<td align="right"></td>
+												  	<td><input type="checkbox" name="pagconf"></input></td>
+												  	<td align="lift">Configuracion pago</td>
+												  	<td align="right"></td>
+												  	<td><input type="checkbox" name="confsop"></input></td>
+												  	<td align="lift">Soporte</td>
+												</tr>
+												<tr>
+												  	<td align="right"></td>
+												  	<td><input type="checkbox" name="cuenadm"></input></td>
+												  	<td align="lift">Cuenta de administracion</td>
+												  	<td align="right"></td>
+												  	<td><input type="checkbox" name="pagvia"></input></td>
+												  	<td align="lift">Via de pago</td>
+													<td align="right"></td>
+													<td><input type="checkbox" name="conface"></input></td>
+												  	<td align="lift">Acerca de</td>
+												</tr>
+												<tr>
+												  	<td align="right"></td>
+												  	<td><input type="checkbox" name="cuennue"></input></td>
+												  	<td align="lift">Nueva cuenta</td>
+												  	<td align="right"></td>
+												  	<td><input type="checkbox" name="pagmon"></input></td>
+												  	<td align="lift">Moneda de pago</td>
+													<td align="right"></td>
+													<td><input type="checkbox" name="confupt"></input></td>
+												  	<td align="lift">Update</td>
+												</tr>
+												<tr>
+												  	<td align="right"></td>
+												  	<td><input type="checkbox" name="cuenbal"></input></td>
+												  	<td align="lift">Hoja de balance</td>
+												  	<td align="right"></td>
+												  	<td><input type="checkbox" name="pagcam"></input></td>
+												  	<td align="lift">Cambio de divisas</td>
+													<td align="right"></td>
+													<td><input type="checkbox" name="confapi"></input></td>
+												  	<td align="lift">APIS</td>
+												</tr>
+												<tr>
+												  	<td align="right"></td>
+												  	<td><input type="checkbox" name="cuendec"></input></td>
+												  	<td align="lift">Declaracion de cuenta</td>
+												  	<td align="right"></td>
+												  	<td><input type="checkbox" name="pagban"></input></td>
+												  	<td align="lift">Cuentas bancarias</td>
+													<td><input type="checkbox" name="tar"></input></td>
+												  	<td align="lift" colspan="2">TAREAS</td>
+												</tr>
+											  </tbody>
+											</table>
+                                        </div>
+                                    </div>
 
                                 <?php } ?>
 
