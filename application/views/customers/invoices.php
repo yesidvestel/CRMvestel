aef74708895ba027d70f4f0a4ba2b592b59e1ae7
Duber1234 <dfpesca@misena.edu.co>
2022-01-05 18:03:29 -0500

    cambio en el titulo del modal

--- application/views/customers/invoices.php
+++ application/views/customers/invoices.php
@@ -342,7 +342,7 @@
             <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span
                             aria-hidden="true">&times;</span></button>
-                <h3 align="center" id="titulo_modal">Crear Nota Debito o Credito</h3>
+                <h3 align="center" id="titulo_modal">Crear Nota Credito o Debito</h3>
                 
             </div>
             <div class="modal-body">
