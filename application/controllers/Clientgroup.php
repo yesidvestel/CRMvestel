8eb7b70973019331d7b9cf3c1b35d929a57a349c
yesidvestel <58863813+yesidvestel@users.noreply.github.com>
2022-01-08 08:09:58 -0500

    CAMBIO EXPORT GRUPOS

--- application/controllers/Clientgroup.php
+++ application/controllers/Clientgroup.php
@@ -569,7 +569,9 @@ class Clientgroup extends CI_Controller
              
         }
         //fin codigo para hacer filtros por mora y servicios
-
+		//$this->db->join('customers', 'tickets.cid=customers.id ', 'left');
+		//$this->db->join('ciudad', 'customers.ciudad=ciudad.idCiudad ', 'left');
+		$this->db->join('barrio', 'customers.barrio=barrio.idBarrio', 'left');
 
         
         $this->load->library('Excel');
