aba4e4bdebc9edfc3a19205e8c21353d70775543
yesidvestel <58863813+yesidvestel@users.noreply.github.com>
2022-01-12 17:42:02 -0500

    cambio

--- index.php
+++ index.php
@@ -71,7 +71,7 @@
  * NOTE: If you change these, also change the error_reporting() code below
  */
 //define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'development');
-define('ENVIRONMENT', 'development');
+define('ENVIRONMENT', 'production');
 
 // we don't want to access the main project before installation. redirect to installation page
 if (ENVIRONMENT === 'pre_installation') {
