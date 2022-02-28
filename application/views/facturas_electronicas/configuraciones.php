c9d0e54f92ecb4ad600f856fe1689d31d419d5e1
Duber1234 <dfpesca@misena.edu.co>
2022-02-28 11:58:19 -0500

    devuelta a 35

--- application/views/facturas_electronicas/configuraciones.php
+++ application/views/facturas_electronicas/configuraciones.php
@@ -169,7 +169,7 @@
                         cuenta_bug++;    
                     }
                     
-                    if(cuenta_bug!=null && cuenta_bug>=77){//estaba en 35 pero para probar un lapso de tiempo mas logebo lo edito si no funciona correctamente es volverlo a 35
+                    if(cuenta_bug!=null && cuenta_bug>=35){//estaba en 35 pero para probar un lapso de tiempo mas logebo lo edito si no funciona correctamente es volverlo a 35
                         
                         console.log("aqui");
                         cuenta_bug=null;
