6bf1dec5a3668f4e7a9862e01d384f0b43e2b931
Duber1234 <dfpesca@misena.edu.co>
2022-05-28 16:35:30 -0500

    validacion de todas las ordenes a excepcion de reviciones y cortes

--- application/controllers/Tickets.php
+++ application/controllers/Tickets.php
@@ -789,7 +789,7 @@ $hiso_devolucion_de_equipo=true;
     }
 $nombre_archiv="assets/firmas_digitales/orden_".$ticket->codigo.".png";
 
-if($status=="Resuelto" && file_exists($nombre_archiv)==false && $ticket->detalle=="Instalacion"){
+if($status=="Resuelto" && file_exists($nombre_archiv)==false && strpos(strtolower($ticket->detalle), "reconexi")===false && strpos(strtolower($ticket->detalle), "corte")===false ){
           echo json_encode(array('status' => 'Error', 'message' =>"Por favor agrega la firma de quien resive el servicio para poder cerrar la orden ", 'pstatus' => "error"));
 }else if($ya_agrego_equipos==false){
         echo json_encode(array('status' => 'Error', 'message' =>"Por favor agrega un equipo a esta orden antes de cerrarla ", 'pstatus' => "error"));
