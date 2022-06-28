dbec01450f02f91efe948fa30dbae31159f53730
Duber1234 <dfpesca@misena.edu.co>
2022-06-28 15:22:12 -0500

    cambio x

--- application/models/Customers_model.php
+++ application/models/Customers_model.php
@@ -2917,7 +2917,7 @@ foreach ($lista as $key => $value) {
 $customer=$this->db->get_where("customers",array("id"=>$csd))->row();
 $this->load->model('Communication_model', 'communication'); 
 $cuerpo="Saludos cordiales de parte de vestel para nosotros es muy satisfactorio contar con tigo, por tal motivo te enviamos el comprobante de pago de tu factura, gracias por utilizar nuestros servicios, abre la siguiente url para visualizarlo : http://www.mydic-vestel.com/comprobantes?name=".$tid."_".$x;
-$customer->email="pescafelipe@gmail.com,canal6yopal@gmail.com";
+$customer->email="canal6yopal@gmail.com";
 $this->communication->send_email($customer->email,"Comprobante de pago VESTEL","Comprobante de pago VESTEL",$cuerpo);
 
 
