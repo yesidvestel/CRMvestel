cde98278c42b7e2a78950d36826f5604d3d8ef68
yesidvestel <canal6yopal@gmail.com>
2022-08-18 17:24:37 -0500

    HISTORIAL CUENTA 3

--- application/views/customers/his_cuenta.php
+++ application/views/customers/his_cuenta.php
@@ -79,6 +79,22 @@
         //datatables
         $('#cgrtable').DataTable({
 			 order: [[2, 'desc']],
+			language: {
+                "lengthMenu": "Mostrar _MENU_ registros",
+                "zeroRecords": "No se encontraron resultados",
+                "info": "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+                "infoEmpty": "Mostrando registros del 0 al 0 de un total de 0 registros",
+                "infoFiltered": "(filtrado de un total de _MAX_ registros)",
+                "sSearch": "Buscar:",
+                "oPaginate": {
+                    "sFirst": "Primero",
+                    "sLast":"Último",
+                    "sNext":"Siguiente",
+                    "sPrevious": "Anterior"
+			     },
+			     "sProcessing":"Procesando...",
+            },
+			
 		});
 			
     });
