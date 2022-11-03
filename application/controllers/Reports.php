fc92969665e1fadafa2affee37cf52c6fb3c59b4
Duber1234 <dfpesca@misena.edu.co>
2022-11-03 10:06:11 -0500

    CIERRE DE CAJA I 3
    correccion

--- application/controllers/Reports.php
+++ application/controllers/Reports.php
@@ -1039,7 +1039,7 @@ public function historial_list(){
                 }
                 $img="";
                 $file_is=null;
-                if($row['type']=="Expense"){
+                if($row['type']=="Expense" && $row['cat']=="Purchase"){
                     $file_is=$this->db->get_where("meta_data",array("type"=>"4","rid"=>$row['tid'],"col2"=>"Pago"))->row();
                 }else{
                     $file_is=$this->db->get_where("meta_data",array("type"=>"77","rid"=>$row['id']))->row();    
