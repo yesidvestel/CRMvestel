181cc508db9db589be899c9814e6ba68d03b4192
yesidvestel <canal6yopal@gmail.com>
2022-12-15 08:49:10 -0500

    ASIGNACIONES 2 G

--- application/views/settings/asignacion.php
+++ application/views/settings/asignacion.php
@@ -14,7 +14,8 @@
                 <div class="col-md-6">
                     <div class="card card-block sameheight-item">
 
-                        <form action="<?php echo base_url() ?>settings/add_asignar" method="post" role="form">
+                        
+							<form id="data_form" method="post" role="form">
                             <div class="form-group row">
                                 <label class="col-sm-3 col-form-label"
                                        for="pay_cat"><?php echo $this->lang->line('') ?>Tipo</label>
@@ -99,7 +100,8 @@
                                 <label class="col-sm-3 col-form-label" for="pay_cat"></label>
 
                                 <div class="col-sm-4">
-                                    <input type="submit" class="btn btn-primary btn-md" value="Actualizar">
+                                    <input type="hidden" value="settings/add_asignar" id="action-url">
+                                    <input  id="submit-data" type="submit" class="btn btn-primary btn-md" value="Actualizar">
 
 
                                 </div>
