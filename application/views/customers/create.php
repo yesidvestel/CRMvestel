bcb044ac1968a553ee0375e117bfe2ea4623a7f4
yesidvestel <canal6yopal@gmail.com>
2023-01-24 15:55:14 -0500

    INTEGRACION VILLAVO 1 I

--- application/views/customers/create.php
+++ application/views/customers/create.php
@@ -529,6 +529,7 @@
     var remote_ip_villanueva_gpon="<?=$ips_remotas['villanueva_gpon']?>";
 	var remote_ip_aguazul="<?=$ips_remotas['aguazul']?>";
 	var remote_ip_tauramena="<?=$ips_remotas['tauramena']?>";
+	var remote_ip_villavo="<?=$ips_remotas['villavo']?>";
     function selecciona_para_agregar(){
         var elemento=document.getElementById("copy_address");
         //console.log($("#discountFormatServicio").val());
@@ -683,6 +684,7 @@ alert(selected);
     var perfil_5 = new Array ("Seleccine...","3Megas","5Megas","5MegasD","10Megas","10MegasSt","10MegasD","15Megas","15MegasD","20Megas","20MegasSt","20MegasD","30Megas","30MegasSt","30MegasD","40Megas","40MegasSt","40MegasD","50Megas","50MegasSt","50MegasD","60Megas","60MegasSt","60MegasD","70Megas","70MegasSt","70MegasD","80Megas","80MegasSt","80MegasD","90Megas","90MegasSt","90MegasD","100Megas","100MegasSt","100MegasD","Cortados");
 	var perfil_6 = new Array ("Seleccine...","3Megas","5Megas","5MegasD","10Megas","10MegasSt","10MegasD","15Megas","15MegasD","20Megas","20MegasSt","20MegasD","30Megas","30MegasSt","30MegasD","40Megas","40MegasSt","40MegasD","50Megas","50MegasSt","50MegasD","60Megas","60MegasSt","60MegasD","70Megas","70MegasSt","70MegasD","80Megas","80MegasSt","80MegasD","90Megas","90MegasSt","90MegasD","100Megas","100MegasSt","100MegasD","Cortados");
 	var perfil_7 = new Array ("Seleccine...","3Megas","5Megas","5MegasD","10Megas","10MegasSt","10MegasD","15Megas","15MegasD","20Megas","20MegasSt","20MegasD","30Megas","30MegasSt","30MegasD","40Megas","40MegasSt","40MegasD","50Megas","50MegasSt","50MegasD","60Megas","60MegasSt","60MegasD","70Megas","70MegasSt","70MegasD","80Megas","80MegasSt","80MegasD","90Megas","90MegasSt","90MegasD","100Megas","100MegasSt","100MegasD","Cortados");
+	var perfil_8 = new Array ("Seleccine...","100MegasR","200MegasR","300MegasR","Cortados");
 							//crear funcion que ejecute el cambio
 							function cambia(){
 								var customergroup;
@@ -730,6 +732,9 @@ alert(selected);
                                 }else if(customergroup=="7"){
                                     $("#Ipremota").val(remote_ip_tauramena);
                                     $("#Ipremota2").val(remote_ip_tauramena);
+                                }else if(customergroup=="8"){
+                                    $("#Ipremota").val(remote_ip_villavo);
+                                    $("#Ipremota2").val(remote_ip_villavo);
                                 }
                                 selecciona_para_agregar();
 							}
@@ -758,6 +763,7 @@ alert(selected);
     var Iplocal_2gpon = new Array ("10.100.0.1");
     var Iplocal_6 = new Array ("10.100.0.1");
     var Iplocal_7 = new Array ("10.100.0.1");
+    var Iplocal_8 = new Array ("10.0.0.1");
 	var Iplocal_3 = new Array ("80.0.0.1");
 	var Iplocal_4 = new Array ("10.1.100.1");
     var Iplocal_3gpon = new Array ("10.20.0.1");
