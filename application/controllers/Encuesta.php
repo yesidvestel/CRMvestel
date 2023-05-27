95ad9c4e31b32a834d62b00101558dc3ad3a6695
Duber1234 <dfpesca@misena.edu.co>
2023-05-26 21:40:47 -0500

    Compatibilidad linux

--- application/controllers/Encuesta.php
+++ application/controllers/Encuesta.php
@@ -18,7 +18,7 @@
 
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class encuesta extends CI_Controller
+class Encuesta extends CI_Controller
 {
 
     public function __construct()
