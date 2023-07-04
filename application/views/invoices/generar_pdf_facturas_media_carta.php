933c75b99d1b55e7ab5e46fb8945a2718e30cc2c
Duber1234 <dfpesca@misena.edu.co>
2023-07-04 14:58:29 -0500

    ENVIO DE CORREOS ESTADO CUENTA 3 G

--- application/views/invoices/generar_pdf_facturas_media_carta.php
+++ application/views/invoices/generar_pdf_facturas_media_carta.php
@@ -175,7 +175,7 @@
     </style>
 </head>
 <body >
-    <?php  ?>
+    <?php if(empty($_GET['clcs'])){ ?>
     <table id="t-titulo" width="100%"  align="center" >
         <tbody>
             <tr>
@@ -193,7 +193,7 @@
 
     <hr>
     <pagebreak>
-    <?php ?>
+    <?php }?>
     
     <?php foreach ($lista as $keyc => $custmr) { 
         $sub_total=0;
@@ -239,7 +239,11 @@ $barrio =$this->db->get_where("barrio",array("idBarrio"=>$custmr->barrio))->row(
             <h6>Telefono : <?= $phoneNumber.$phoneNumber2 ?></h6>
             <h6 style="line-height:10px;font-size: 8px;padding-top: -23px;padding-right: 3px;">Reciba un cordial saludo de parte de <?=$company->cname ?>, a continuación, presentamos el documento de cobro de sus servicios hogar.</h6>
         </div>
+        <?php if($_SESSION[md5("variable_datos_pin")]['url'] =="https://www.vesteldigital.com.co"){ ?>
+        <div id="logo-div"> <img id="logo" src="<?=FCPATH."userfiles/company/".$company->logo  ?>"></div>
+    <?php }else{ ?>
         <div id="logo-div"> <img id="logo" src="<?=base_url()."userfiles/company/".$company->logo  ?>"></div>
+    <?php } ?>
     <!-- Encabezado-->
     <br>
     <!--Cuerpo Facturas-->
