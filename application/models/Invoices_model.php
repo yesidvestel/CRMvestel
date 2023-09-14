53cc5b5a5fa1bc939411638a5d39849f8bc7847c
Duber1234 <dfpesca@misena.edu.co>
2023-09-14 11:29:46 -0500

    SERVICIOS REVERT 1 G
    se revierten cambios para solucion temporal

--- application/models/Invoices_model.php
+++ application/models/Invoices_model.php
@@ -573,11 +573,11 @@ setlocale(LC_TIME, "spanish");
     public function get_servicios(){
         //$array=array();//array("servicios_tv"=>array(),"servicios_internet"=>array());
         $lista_sedes=$this->db->get_where("customers_group")->result_array();
-        $lista_sedes[]['id']="0";
-        $lista_sedes[]['id']="0";
+        $lista_sedes[]['id']="-1";
+        $lista_sedes[]['id']="-2";
         foreach($lista_sedes as $key=> $value){
-            $lista_sedes[$key]['servicios_tv']=$this->db->get_where("products",array("pertence_a_tv_o_net"=>"Tv"))->result_array();
-            $lista_sedes[$key]['servicios_internet']=$this->db->get_where("products",array("pertence_a_tv_o_net"=>"Internet"))->result_array();
+            $lista_sedes[$key]['servicios_tv']=$this->db->get_where("products",array("pcat"=>"1","warehouse"=>"1","sede"=>$value['id'],"valores!="=>null,"pertence_a_tv_o_net"=>"Tv"))->result_array();
+            $lista_sedes[$key]['servicios_internet']=$this->db->get_where("products",array("pcat"=>"1","warehouse"=>"1","sede"=>$value['id'],"valores!="=>null,"pertence_a_tv_o_net"=>"Internet"))->result_array();
             foreach ($lista_sedes[$key]['servicios_tv'] as $key2 => $value2) {
                 
                 $x1=explode("-", $value2['valores']);
