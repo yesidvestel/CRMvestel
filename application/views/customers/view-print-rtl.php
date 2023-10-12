8bc414806b0cca2b0d3abc5580cd8d787d72d061
yesidvestel <canal6yopal@gmail.com>
2023-10-12 10:52:30 -0500

    CAMBIOS CONTRATO OTTIS 2
    alineacion logo

--- application/views/customers/view-print-rtl.php
+++ application/views/customers/view-print-rtl.php
@@ -621,7 +621,7 @@ $fcontrato = $details['f_contrato'];
 			</table>
 			<table>
 				<tr>
-					<td ><img src="<?php echo FCPATH . 'userfiles/company/' . $this->config->item('logo') ?>" style="max-width:50%;"></td>
+					<td ><img src="<?php echo FCPATH . 'userfiles/company/' . $this->config->item('logo') ?>" style="max-width:50%;align-content: center"></td>
 				</tr>
 			</table>
 			<table>
