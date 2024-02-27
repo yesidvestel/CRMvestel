fd12edbcf62faefdd4d499e0150711a7cd9ed07e
Duber1234 <dfpesca@misena.edu.co>
2024-02-27 11:36:05 -0500

    SAVES V10.9 FE 2

--- application/controllers/FacturasElectronicas.php
+++ application/controllers/FacturasElectronicas.php
@@ -430,9 +430,9 @@ $this->load->model("customers_model","customers");
             }
         }else if($dataApiNET!=null && $dataApiNET!="null"){
             $ob1=$this->db->get_where("config_facturacion_electronica",array("id"=>2))->row();
-            //$retorno = $api->accionar2($api,$dataApiNET,$ob1->tocken);         
+            $retorno = $api->accionar2($api,$dataApiNET,$ob1->tocken);         
         }
-$retorno['mensaje']="Factura Guardada";
+//$retorno['mensaje']="Factura Guardada";
         if($retorno['mensaje']=="Factura Guardada"){
              //$dataInsert["json"]=$dataApiNET."  output: ".$retorno['respuesta'];
         	$this->db->insert("facturacion_electronica_siigo",$dataInsert);
