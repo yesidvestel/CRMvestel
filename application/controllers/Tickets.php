e18171e08f771a657970bdfe14335d32dd841fc8
Duber1234 <dfpesca@misena.edu.co>
2024-04-25 15:19:40 -0500

    SAVES V10.38
    correccion

--- application/controllers/Tickets.php
+++ application/controllers/Tickets.php
@@ -1953,7 +1953,7 @@ $x=0;
         $y=0;
         $itemx1=$this->db->get_where("invoice_items",array("tid"=>$idfactura,"product"=>$factura->combo))->row();
                 if($data['combo']!=='no' && empty($itemx1)){
-                    $producto = $this->db->get_where('products',array('product_name'=>$factura->combo))->row();
+                    $producto = $this->db->get_where('products',array('product_name'=>$temporal->internet))->row();
 					$datay['pid']=$producto->pid;
                     $x=intval($producto->product_price);
                     $x=($x/31)*$diferencia->days;
