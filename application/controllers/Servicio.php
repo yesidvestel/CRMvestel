36dfd9b45fbc80c509af954669a0c1e3cced403a
Duber1234 <dfpesca@misena.edu.co>
2024-10-08 21:12:59 -0500

    SAVES V11.44-5

--- application/controllers/Servicio.php
+++ application/controllers/Servicio.php
@@ -130,7 +130,7 @@ class Servicio extends CI_Controller
         $data_response['data_customer']=$this->db->get_where("customers",array("id"=>$body_post->cid))->row();
         $id_ultima_factura=$this->db->query("SELECT promo_sistema_clientes1 as promo_sistema_clientes1 from invoices where csd=".$body_post->cid." order by tid desc limit 1")->result_array();
         $data_response['data_promos']=$id_ultima_factura[0]['promo_sistema_clientes1'];
-        $data_response['data_estados_promos']= $this->db->customers->validar_promocion_estado_cus($body_post->cid);
+        $data_response['data_estados_promos']= $this->customers->validar_promocion_estado_cus($body_post->cid);
         echo json_encode($data_response);
     }
 
