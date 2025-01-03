1b9bdd31233d5a53ae371c1541e031a50564cdf3
Duber1234 <dfpesca@misena.edu.co>
2025-01-03 16:41:52 -0500

    SAVES V11.67
    error en ticket 213101 doris yanira solucionado

--- application/controllers/Tickets.php
+++ application/controllers/Tickets.php
@@ -1015,7 +1015,7 @@ if($_SESSION[md5("variable_datos_pin")]['db_name'] != "admin_crmvestel" && $stat
         $datay['discount']=0;
         
         $datay['totaldiscount']=0;			
-                if($data['combo']!=="no" || $inter==="no"){
+                if($data['combo']!=="no" || $inter!=="no"){
                     $producto = $this->db->get_where('products',array('product_name'=>$data['combo']))->row();
 					if($this->config->item('ctitle')!='FUTURE SOLUTIONS DEVELOPMENT SAS' && $ticket->detalle=="Instalacion"){
 						$x=0;
