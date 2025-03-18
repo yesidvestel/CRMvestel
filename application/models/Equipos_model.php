86dea1dceebfdbd86d9b4f788f2f711db56c40fe
yesidvestel <canal6yopal@gmail.com>
2025-03-18 15:29:31 -0500

    SAVES V12.02 - 2

--- application/models/Equipos_model.php
+++ application/models/Equipos_model.php
@@ -39,7 +39,6 @@ class Equipos_model extends CI_Model
 		 	//$this->db->join('puertos', 'puertos.idp = equipos.puerto','left');
 		 	$this->db->join('naps', 'naps.idn = equipos.nat','left');
 		 	$this->db->join('vlans', 'vlans.idv = naps.idvlan','left');
-		 	$this->db->join('almacen_equipos', 'almacen_equipos.id = equipos.almacen','left');
 		 	
 			
 		 if ($id > 0) {
